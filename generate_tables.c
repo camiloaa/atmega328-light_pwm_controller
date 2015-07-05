9cf2fdde8fd7b51508c61150f7e79315894b099c
Camilo Alejandro Arboleda <camilo@ieee.org>
2015-07-05 11:17:09 +0200

    New table generation

--- generate_tables.c
+++ generate_tables.c
@@ -6,57 +6,80 @@
  *      linear_log = Linear until log catches
  *      average = Average between linear_log and log until log catches
  *      log = Logarithmic scale
+ * Input
+ *      top: Maximum value in X-axis
+ *      max: Maxumum value in Y-axis
+ *      nom,den: Nominator and denominator such 'max = e^(top*nom/den) - 1
+ *               Has to be calculated by a calculator because i don't know how to do it myself.
+ *      step: Step for the linear part in the linear+log curve
  * */
 void genarray(int *array, int top, int max, double nom, double den, double step) {
-	int j;
-	double x;
-	double w = 0;
-	for (j=0; j<top; j++) {
-		x = j;
-		double e = x*nom/den;
-		double y = exp(e) - 1;
-		array[j] = j*max/top; // Full linear
-		array[j+3*top] = round(y); // Full log
-		if (y < w) { // Log line hasn't catched 'average' line
-			array[j+top] = round(w); // Log after slow linear
-			array[j+2*top] = round((w+y)/2); // Log after weighed linear/log
-		} else {
-			array[j+top] = array[j+2*top] = round(y);
-		}
-		w += step;
-	}
+    int j;
+    double x;
+    double w = 0;
+    for (j = 0; j < top; j++) {
+        x = j;
+        double e = x * nom / den;
+        double y = exp(e) - 1;
+        array[j] = j * max / top; // Full linear
+        array[j + 3 * top] = round(y); // Full log
+        if (y < w) { // Log line hasn't catched 'average' line
+            array[j + top] = round(w); // Log after slow linear
+            array[j + 2 * top] = round((w + y) / 2); // Log after weighed linear/log
+        } else {
+            array[j + top] = array[j + 2 * top] = round(y);
+        }
+        w += step;
+    }
 
 }
 
-void printarray(int *array, int top, int split) {
-	int j, k;
-	printf("{");
-	for (k=0; k<4; k++) {
-		printf("{");
-		for (j=0; j<top; j++) {
-			if (0 == j%split) printf("\n");
-			printf("%3d", array[j+top*k]);
-			if (j != top-1) printf(",");
-		}
-		printf("}\n");
-	}
-	printf("}\n");
+void printarray(int *array, int top, int base, int split) {
+    int j, k;
+    printf("{");
+    for (k = 0; k < 4; k++) {
+        printf("{");
+        for (j = 0; j < top; j++) {
+            if (0 == j % split)
+                printf("\n");
+            printf("%3d", array[j + top * k]);
+            if (j != top - 1)
+                printf(",");
+        }
+        printf("}\n");
+        printf("{");
+        for (j = 0; j < top; j++) {
+            if (0 == j % split)
+                printf("\n");
+            printf("%3d", base - array[j + top * k]);
+            if (j != top - 1)
+                printf(",");
+        }
+        printf("}\n");
+    }
+    printf("}\n");
 }
 
+/* Experimented with different values trying to match the curve showed in
+ * http://what-when-how.com/display-interfaces/the-human-visual-system-display-interfaces-part-2/
+ * A 0.4 step gave best results
+ * */
 int main() {
-	int j,k;
-	double x=0;
-	double w = 0;
-	int    yi = 0;
-	double r100_101 = 207818/4502981; // 100
-	double r255_101 = 130729/2357526; // 255
-	int base100[101*4];
-	int base127[128*4];
+    int j, k;
+    double x = 0;
+    double w = 0;
+    int yi = 0;
+    double r100_101 = 207818 / 4502981; // 100
+    double r255_101 = 130729 / 2357526; // 255
+    int base100[101 * 4];
+    int base127[128 * 4];
+    int base255[256 * 4];
 
-	genarray(base100, 101, 100, 207818, 4502981, 0.5);
-	genarray(base127, 128, 127, 53273, 1394400, 0.5); // Translate 127->127
-	genarray(base127, 128, 256, 53273, 1220100, 0.4); // Translate 127->255
-	printarray(base127, 128, 16);
-	return 0;
+    genarray(base100, 101, 100, 207818, 4502981, 0.5);
+    // genarray(base127, 128, 127, 53273, 1394400, 0.5); // Translate 127->127
+    genarray(base127, 128, 256, 53273, 1220100, 0.4); // Translate 127->255
+    genarray(base255, 256, 256, 44553, 2048810, 0.5); // Translate 255->255
+    printarray(base255, 256, 255, 16);
+    return 0;
 }
 
